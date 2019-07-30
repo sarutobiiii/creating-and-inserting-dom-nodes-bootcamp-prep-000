70f8f18fc76bcb48e40fed9ff0855e6d66f112b8
oluwatobi balogun <tobibags19@gmail.com>
2019-07-30 02:53:15 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -3,4 +3,16 @@
 const codes = [
   "ArrowUp",
   "ArrowUp",
-  ]
+  "ArrowDown",
+  "ArrowDown",
+  "ArrowLeft",
+  "ArrowRight",
+  "ArrowLeft",
+  "ArrowRight",
+  "b",
+  "a",
+  ];
+  
+  function init() {
+    
+  }
